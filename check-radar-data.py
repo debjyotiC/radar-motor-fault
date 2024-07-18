8058fc5e34071489d41f725db986366347898c3e
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2024-07-18 14:15:44 +0530

    classifier

--- check-radar-data.py
+++ check-radar-data.py
@@ -2,28 +2,10 @@ import numpy as np
 from os import listdir
 import matplotlib.pyplot as plt
 
-data = np.load("data/npz_files/radar-motor.npz", allow_pickle=True)
+data = np.load("data/npz_files/radar-cfar-motor.npz", allow_pickle=True)
 class_labels = listdir("data/radar-motor")
 
 motor_data, motor_label = data['out_x'], data['out_y']
-
-
-def apply_2d_cfar(signal, guard_band_width, kernel_size, threshold_factor):
-    num_rows, num_cols = signal.shape
-    threshold_signal = np.zeros((num_rows, num_cols))
-    for i in range(guard_band_width, num_rows - guard_band_width):
-        for j in range(guard_band_width, num_cols - guard_band_width):
-            # Estimate the noise level
-            noise_level = np.mean(np.concatenate((
-                signal[i - guard_band_width:i + guard_band_width, j - guard_band_width:j + guard_band_width].ravel(),
-                signal[i - kernel_size:i + kernel_size, j - kernel_size:j + kernel_size].ravel())))
-            # Calculate the threshold for detection
-            threshold = threshold_factor * noise_level
-            # Check if the signal exceeds the threshold
-            if signal[i, j] > threshold:
-                threshold_signal[i, j] = 1
-    return threshold_signal
-
 
 for count, frame in enumerate(motor_data):
     plt.clf()
