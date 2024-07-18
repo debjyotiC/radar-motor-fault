a01adfc314725fde4a0f79f43bbe8782d5aa752d
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2024-07-18 13:59:34 +0530

    initial commit

--- check-radar-data.py
+++ check-radar-data.py
@@ -7,8 +7,27 @@ class_labels = listdir("data/radar-motor")
 
 motor_data, motor_label = data['out_x'], data['out_y']
 
+
+def apply_2d_cfar(signal, guard_band_width, kernel_size, threshold_factor):
+    num_rows, num_cols = signal.shape
+    threshold_signal = np.zeros((num_rows, num_cols))
+    for i in range(guard_band_width, num_rows - guard_band_width):
+        for j in range(guard_band_width, num_cols - guard_band_width):
+            # Estimate the noise level
+            noise_level = np.mean(np.concatenate((
+                signal[i - guard_band_width:i + guard_band_width, j - guard_band_width:j + guard_band_width].ravel(),
+                signal[i - kernel_size:i + kernel_size, j - kernel_size:j + kernel_size].ravel())))
+            # Calculate the threshold for detection
+            threshold = threshold_factor * noise_level
+            # Check if the signal exceeds the threshold
+            if signal[i, j] > threshold:
+                threshold_signal[i, j] = 1
+    return threshold_signal
+
+
 for count, frame in enumerate(motor_data):
     plt.clf()
-    plt.title(f"Frame no. {count} has label {class_labels[motor_label[count]-1]}")
+    # frame = apply_2d_cfar(frame, guard_band_width=2, kernel_size=5, threshold_factor=1)
+    plt.title(f"Frame no. {count} has label {class_labels[motor_label[count] - 1]}")
     plt.contourf(frame)
-    plt.pause(0.5)
+    plt.pause(1)
--- parse-radar-data.py
+++ parse-radar-data.py
@@ -71,7 +71,7 @@ for folder in range(len(all_targets)):
 
         for col in df_data.columns:
             data = calc_range_doppler(df_data, col, configParameters)
-            cfar_data = apply_2d_cfar(data, guard_band_width=3, kernel_size=5, threshold_factor=1)
+            cfar_data = apply_2d_cfar(data, guard_band_width=2, kernel_size=5, threshold_factor=1)
 
             out_x_range_doppler.append(data)
             out_x_range_doppler_cfar.append(cfar_data)
